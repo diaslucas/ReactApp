1a79c59d8e033064d012f46a41d20b1633ae1030
diaslucas <diaslucas>
2018-08-31 22:22:23 -0400

    Implementing navigation menu and users list

---
+++ src/AddUser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddUser extends Component {
+  render() {
+    return (
+        <h1>Add users </h1>
+    );
+  }
+}
+
+export default AddUser;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import AddUser from './AddUser';
+import UsersList from './UsersList';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Testing</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <Link to='/' className="nav-link">Users List</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/AddUser' className="nav-link">Add User</Link>
+            </li>
+          </ul>
+
+          <Route exact path='/' component={UsersList} />
+          <Route path='/AddUser' component={AddUser} />
+        </div>
+      </Router>
     );
   }
 }
