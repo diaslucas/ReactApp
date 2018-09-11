a0b3ee0ef2adcf3141d35d4107937b73f7972a5a
diaslucas <diaslucas>
2018-09-10 21:16:34 -0400

    Adding an underline for the active menu

--- src/App.js
+++ src/App.js
@@ -5,17 +5,52 @@ import UsersList from './Components/UsersList';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      menus: [
+        { menuName: 'Users List', menuLink: '/', active: true },
+        { menuName: 'Add User', menuLink: '/AddUser', active: false }
+      ]
+    }
+  }
+
+  Menu(props) {
+    let activeClass = (props.active ? 'menu-active' : '');
+    return (
+      <li className={"nav-item " + activeClass}>
+        <Link to={props.link} className="nav-link" onClick={props.onClick}>{props.text}</Link>
+      </li>
+    )
+  }
+
+  SetMenuActive(index) {
+    let menus = this.state.menus;
+    menus.forEach((menu, i) => {
+      menu.active = (i === index);
+    });
+    this.setState({
+      menus: menus
+    });
+  }
+
   render() {
+    const { menus } = this.state;
     return (
       <Router>
         <div>
           <ul className="nav nav-pills">
-            <li className="nav-item">
-              <Link to='/' className="nav-link">Users List</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/AddUser' className="nav-link">Add User</Link>
-            </li>
+            {menus.map((menu, index) => {
+              return (
+                <this.Menu
+                  key={index}
+                  link={menu.menuLink}
+                  text={menu.menuName}
+                  active={menu.active}
+                  onClick={() => this.SetMenuActive(index)}
+                />);
+            })}
           </ul>
 
           <Route exact path='/' component={UsersList} />
