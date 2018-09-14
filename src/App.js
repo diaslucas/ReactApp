03b0b0a2c8bf50f39aa33d2eb67cb41a6603a3f9
diaslucas <diaslucas>
2018-09-13 22:08:45 -0400

    Using activeClassName to underline active menu tab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
 import AddUser from './Components/AddUser';
 import UsersList from './Components/UsersList';
 import TestForm from './Components/TestForm';
@@ -11,30 +11,19 @@ class App extends Component {
     super(props)
     this.state = {
       menus: [
-        { menuName: 'Users List', menuLink: '/', active: true },
-        { menuName: 'Add User', menuLink: '/AddUser', active: false },
-        { menuName: 'Test', menuLink: '/Test', active: false }
+        { menuName: 'Users List', menuLink: '/' },
+        { menuName: 'Add User', menuLink: '/AddUser' },
+        { menuName: 'Test', menuLink: '/Test' }
       ]
     }
   }
 
-  Menu(props) {
-    let activeClass = (props.active ? 'menu-active' : '');
+  Menus(props) {
     return (
-      <li className={"nav-item " + activeClass}>
-        <Link to={props.link} className="nav-link" onClick={props.onClick}>{props.text}</Link>
+      <li className="nav-item">
+        <NavLink to={props.link} exact activeClassName="menu-active" className="nav-link">{props.text}</NavLink>
       </li>
     )
-  }
-
-  SetMenuActive(index) {
-    let menus = this.state.menus;
-    menus.forEach((menu, i) => {
-      menu.active = (i === index);
-    });
-    this.setState({
-      menus: menus
-    });
   }
 
   render() {
@@ -42,15 +31,13 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <ul className="nav nav-pills">
+          <ul className="nav">
             {menus.map((menu, index) => {
               return (
-                <this.Menu
+                <this.Menus
                   key={index}
                   link={menu.menuLink}
                   text={menu.menuName}
-                  active={menu.active}
-                  onClick={() => this.SetMenuActive(index)}
                 />);
             })}
           </ul>
